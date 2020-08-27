22966ddd16f75898358c54f6c13c3b090bc68c9a
Charvi04 <66305846+Charvi04@users.noreply.github.com>
2020-08-27 11:18:31 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -67,8 +67,8 @@ class Game{
                     
                          textSize(25);
                          fill("white");
-                         text("Player 1 :" +allPlayers.player1.score,50,50);
-                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
+                         text("allPlayers[plr].name : " +allPlayers.player1.score,50,50);
+                        text("allPlayers[plr].name : " + allPlayers.player2.score, 50, 100);
                  
                  }
                 
